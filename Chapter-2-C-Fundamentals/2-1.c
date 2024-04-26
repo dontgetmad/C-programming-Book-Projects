01af04d385b562236197865e2f389b0b5adf783b
dontgetmad <168203658+dontgetmad@users.noreply.github.com>
2024-04-26 17:06:23 +0100

    Create 2-1.c

---
+++ Chapter-2-C-Fundamentals/2-1.c
@@ -0,0 +1,20 @@
+/*
+ * Name: 2.1.c
+ * Purpose: learning C
+ * Author: dontgetmad
+ * Copyright (c) 2024 Author. All Rights Reserved.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define ASTERIX "*"
+
+int main() {
+  printf("        %s\n", ASTERIX);
+  printf("       %s\n", ASTERIX);
+  printf("      %s\n", ASTERIX);
+  printf("%s    %s\n", ASTERIX, ASTERIX);
+  printf(" %s  %s\n", ASTERIX, ASTERIX);
+  printf("   %s\n", ASTERIX);
+}
