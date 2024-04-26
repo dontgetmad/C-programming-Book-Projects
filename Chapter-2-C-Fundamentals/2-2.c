1b3a2a318bae19cf1e927584c649077ade2e186a
dontgetmad <168203658+dontgetmad@users.noreply.github.com>
2024-04-26 17:16:45 +0100

    Create 2-2.c

---
+++ Chapter-2-C-Fundamentals/2-2.c
@@ -0,0 +1,30 @@
+// Question
+/*
+ * Write a program that computes the volume of a sphere with a 10-meter radius, using the for- mula v = 4/3πr3. 
+ * Write the fraction 4/3 as 4.0f/3.0f. (Try writing it as 4/3. What hap- pens?) 
+ * Hint: C doesn’t have an exponentiation operator, so you’ll need to multiply r by itself twice to compute r3.
+*/
+
+/*
+ * Name: 2.2.c
+ * Purpose: learning C
+ * Author: dontgetmad
+ * Copyright (c) 2024 Author. All Rights Reserved.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define PI 3.14
+
+int main() {
+  float volume, r;
+
+  printf("---- This program calculates the volume of a sphere ----\n");
+
+  printf("Enter a radius (meters): ");
+  scanf("%f", &r);
+
+  volume = (4.0f / 3.0f) * (PI * (r * r * r));
+  printf("Volume of sphere (meteres): %.2f\n", volume);
+}
