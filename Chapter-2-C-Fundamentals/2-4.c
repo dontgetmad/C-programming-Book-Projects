0197d1a46e60e5e7e04c5ebb79e2d10c36607a47
dontgetmad <168203658+dontgetmad@users.noreply.github.com>
2024-04-26 17:38:46 +0100

    Create 2-4.c

---
+++ Chapter-2-C-Fundamentals/2-4.c
@@ -0,0 +1,28 @@
+// Question
+/*
+  * Write a program that asks the user to enter a dollars-and-cents amount, then displays the amount with 5% tax added:
+  * Enter an amount: 100.00
+  * With tax added: $105.00
+*/
+
+/*
+ * Name: 2.4.c
+ * Purpose: learning C
+ * Author: dontgetmad
+ * Copyright (c) 2024 Author. All Rights Reserved.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  float amount, percentage_to_add, final_bill;
+
+  printf("Enter total amount (U.S. dollars): ");
+  scanf("%f", &amount);
+  printf("Enter percentage to add: ");
+  scanf("%f", &percentage_to_add);
+
+  final_bill = amount * (1 + (percentage_to_add / 100));
+  printf("With tax added: $%.2f\n", final_bill);
+}
