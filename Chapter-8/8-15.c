5c720a67cd51e774eb76f6596c79422cf6ca3812
dontgetmad <168203658+dontgetmad@users.noreply.github.com>
2024-06-06 15:13:26 +0100

    Update 8-15.c

--- Chapter-8/8-15.c
+++ Chapter-8/8-15.c
@@ -1,4 +1,22 @@
-// WORK IN PROGRESS
+/*
+One of the oldest known encryption techniques is the Caesar cipher, attributed to Julius Cae- sar. 
+It involves replacing each letter in a message with another letter that is a fixed number of
+positions later in the alphabet. (If the replacement would go past the letter Z, the cipher “wraps around” to the beginning of the alphabet. 
+For example, if each letter is replaced by the letter two positions after it, then Y would be replaced by A, and Z would be replaced by B.) 
+Write a program that encrypts a message using a Caesar cipher. The user will enter the message to be encrypted and the shift amount (the number of positions by which letters should be shifted):
+    Enter message to be encrypted: Go ahead, make my day.
+    Enter shift amount (1-25): 3
+    Encrypted message: Jr dkhdg, pdnh pb gdb.
+Notice that the program can decrypt a message if the user enters 26 minus the original key:
+    Enter message to be encrypted: Jr dkhdg, pdnh pb gdb.
+    Enter shift amount (1-25): 23
+    Encrypted message: Go ahead, make my day.
+You may assume that the message does not exceed 80 characters. Characters other than let- ters should be left unchanged. 
+Lower-case letters remain lower-case when encrypted, and upper-case letters remain upper-case. Hint: 
+To handle the wrap-around problem, use the expression((ch-'A')+n)%26+'A'to calculate the encrypted version of an upper- case letter, where ch stores the letter and n stores the shift amount. (
+You’ll need a similar expression for lower-case letters.)
+*/
+
 
 /*
  * Name: 8-15.c
@@ -34,20 +52,36 @@ int main(void) {
 
   printf("Enter shift amount (1-25): ");
   scanf("%d", &shift_amount);
+  printf("Encrypted message: ");
+
+  if (shift_amount < 1 || shift_amount > 25) {
+    printf(
+        "Shift amount entered is too small or too large, try agian! (1-25)\n");
+    return 0;
+  }
 
   for (i = 0; i < ARR_SIZE; i++) {
 
-    if (input_sentence[i] == '\0') {
+    if (!isalpha(input_sentence[i])) {
+      printf("%c", input_sentence[i]);
       continue;
     }
 
-    printf("%c\n", input_sentence[i]);
-    temp = (input_sentence[i] - 'a') + shift_amount;
-    if (temp > 26) {
-      temp = temp % 26;
+    if (toupper(input_sentence[i]) != input_sentence[i]) {
+      temp = (input_sentence[i] - 'a') + shift_amount;
+      if (temp >= 26) {
+        temp = temp % 26;
+        encrypted_message[i] = alphabet[temp];
+      }
       encrypted_message[i] = alphabet[temp];
+    } else {
+      temp = (input_sentence[i] - 'A') + shift_amount;
+      if (temp >= 26) {
+        temp = temp % 90;
+        encrypted_message[i] = toupper(alphabet[temp]);
+      }
+      encrypted_message[i] = toupper(alphabet[temp]);
     }
-    encrypted_message[i] = alphabet[temp];
     printf("%c", encrypted_message[i]);
   }
   printf("\n");
